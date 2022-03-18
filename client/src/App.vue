04a8d7dce506159d1a454f5ea2b4f9161464c99a
caubesin <caubessin@gmail.com>
2022-03-18 18:27:58 +0700

    fix bug

---
+++ client/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <GoogleMap />
+  </div>
+</template>
+
+<script>
+import GoogleMap from "./components/GoogleMap.vue";
+
+export default {
+  name: "App",
+  components: {
+    GoogleMap,
+  },
+  mounted() {
+    console.log("app mounted");
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
